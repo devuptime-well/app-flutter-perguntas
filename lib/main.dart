fbe7336f109e6728e07a9595261a67a067937a19
devuptime-well <welq2005@gmail.com>
2020-12-11 16:33:17 -0300

    Column - RaiseButon - Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é sua cor favorita?',
+      'Qual é o seu animal favorito',
+    ];
     return MaterialApp(
-      home: Text('Olá flutter!!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
